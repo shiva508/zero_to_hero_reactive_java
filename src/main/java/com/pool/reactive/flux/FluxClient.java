509e094a06638686ad41d9ae1a573639a320aa37
shiva508 <dasarishiva1@gmail.com>
2022-11-16 22:31:44 +0530

    react native

--- src/main/java/com/pool/reactive/flux/FluxClient.java
+++ src/main/java/com/pool/reactive/flux/FluxClient.java
@@ -1,5 +1,6 @@
 package com.pool.reactive.flux;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -11,11 +12,23 @@ public class FluxClient {
 		Flux<String> publisher = Flux.just("Superman", "Batman", "Death Stroke");
 		Consumer<String> consumer = System.out::println;
 		publisher.subscribe(consumer);
-		
+
 		List<String> names = Arrays.asList("Hulk", "Ironman", "Antman");
 		Flux<String> collectionToFluxPublisher = Flux.fromIterable(names);
 		collectionToFluxPublisher.subscribe(consumer);
-		
-		
+
+		List<String> words = Arrays.asList("the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog");
+
+		Flux.fromIterable(words)
+				.flatMap(word -> Flux.fromArray(word.split("")))
+				.distinct()
+				.sort()
+				.zipWith(Flux.range(1, 100), (word, cound) -> cound + ". " + word)
+				.subscribe(consumer);
+		Flux<String> fastClock = Flux.interval(Duration.ofMillis(1)).map(tick -> "fast " + tick);
+		Flux<String> slowClock = Flux.interval(Duration.ofSeconds(2)).map(tick -> "fast" + tick);
+		Flux<String> clock = Flux.merge(fastClock, slowClock);
+		clock.subscribe(System.out::println);
+
 	}
 }
